2abb198ccdde27d0230165f523ddf7955cc1627d
95tuanle <tlna95@icloud.com>
2023-11-18 01:19:25 -0500

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+states_to_capitals = {
+    "Texas": "Austin",
+    "New York": "Albany"
+}
+
+print(states_to_capitals["New York"])
+
+for key, value in states_to_capitals.items():
+    print(key + " | " + value)
