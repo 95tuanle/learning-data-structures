85cf2553b32537942d99e455169c1d8b84fe60c0
95tuanle <tlna95@icloud.com>
2023-11-20 06:46:31 -0500

    set

---
+++ set.py
@@ -0,0 +1,12 @@
+primary_colors = {'red', 'blue', 'yellow'}
+
+color = "green"
+
+if color in primary_colors:
+    print(f"{color} is a primary color.")
+else:
+    print(f"{color} is not a primary color.")
+
+letters = {"a", "b"}
+letters.add("c")
+print(letters)
